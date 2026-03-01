fcf2ecdc440696a7c2b6c7ef0db19affa1c7dcc3
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2026-03-01 17:47:16 +0530

    Add files via upload

---
+++ daligates/daligates/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace practiceapp
+{
+
+	class Program
+	{
+		class hello
+		{
+
+			public delegate void helloHandler();
+			public void welcom()
+			{
+				Console.WriteLine("Hello And Welcome To C# and .Net");
+			}
+		}
+		static void Main(string[] args)
+		{
+			hello h = new hello();
+			hello.helloHandler wh = new hello.helloHandler(h.welcom);
+			wh();
+		}
+	}
+}
