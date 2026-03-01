6f71d47ac6dcae18f1bb3f8989f13df7c8a9b30a
Vinayak Bosamiya <vinaybosamiya294@gmail.com>
2026-03-01 17:46:21 +0530

    Add files via upload

---
+++ class with in obj/class with in obj/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace stud
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			student.s();
+			//student[] obj = new student[3];
+			//for(int i = 0; i < obj.Length; i++)
+			//{
+			//	obj[i] = new student();
+			//	Console.WriteLine("Enter the roll no: ");
+			//	int rno = int.Parse(Console.ReadLine());
+
+			//	Console.WriteLine("Enter the fee: ");
+			//	int fee = int.Parse(Console.ReadLine());
+
+			//	obj[i].set(rno,fee);
+			//}
+
+			//for (int i = 0; i < obj.Length; i++)
+			//{
+			//	if (obj[i].f <= 1000)
+			//	{
+			//		Console.WriteLine("you are not aligible for this school rollNo : " + obj[i].r);
+			//	}
+			//	else
+			//	{
+
+			//	obj[i].display();
+			//	}
+			//}
+
+		}
+	}
+}
---
+++ class with in obj/class with in obj/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace stud
+{
+
+public class student
+{
+		static public void s()
+		{
+			Console.WriteLine("i am static class");
+		}
+		public int r,f;
+		public void set(int r,int f)
+		{
+			this.r = r;
+			this.f = f;
+		}
+
+		public void display()
+		{
+			Console.WriteLine("the roll no: {0} and fees: {1}",r,f);
+		}
+}
+
+}
